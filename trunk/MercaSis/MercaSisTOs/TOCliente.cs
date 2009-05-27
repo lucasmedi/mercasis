2d2918fd0a4c5c2c884363af31c5ef48bee90952
lucas.cunha2104 <lucas.cunha2104@a6ead2e0-2a24-11de-8010-b76041e17fd7>
2009-05-27 23:58:53 +0000

    Vai caralho

---
+++ trunk/MercaSis/MercaSisTOs/TOCliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MercaSisTOs
+{
+    /// <summary>
+    /// Classe TOCliente com as instâncias dos objetos com os dados do Cliente
+    /// </summary>
+    public class TOCliente
+    {
+        public Campo<long> Codigo = new Campo<long>();
+        public Campo<string> NomeCompleto = new Campo<string>();
+        public Campo<string> Cpf_Cnpj = new Campo<string>();
+        public Campo<string> Sexo = new Campo<string>();
+        public Campo<DateTime> DataNascimento = new Campo<DateTime>();
+        public Campo<DateTime> DataCadastro = new Campo<DateTime>();
+        public Campo<string> Telefone = new Campo<string>();
+        public Campo<string> TelefoneSec = new Campo<string>();
+        public Campo<string> Email = new Campo<string>();
+        public Campo<string> Senha = new Campo<string>();
+        public Campo<string> Endereco = new Campo<string>();
+        public Campo<int> Numero = new Campo<int>();
+        public Campo<string> Complemento = new Campo<string>();
+        public Campo<string> Cep = new Campo<string>();
+        public Campo<string> Bairro = new Campo<string>();
+        public Campo<string> Cidade = new Campo<string>();
+        public Campo<string> Estado = new Campo<string>();
+        public Campo<string> Pais = new Campo<string>();
+    }
+}
---
+++ trunk/MercaSis/MercaSisTOs/TOPedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MercaSisTOs
+{
+    public class TOPedido
+    {
+        public Campo<int> CodigoPedido = new Campo<int>();
+        public Campo<int> CodigoCliente = new Campo<int>();
+        public Campo<string> EnderecoEntrega = new Campo<string>();
+        public Campo<int> NumeroEntrega = new Campo<int>();
+        public Campo<string> BairroEntrega = new Campo<string>();
+        public Campo<string> ComplementoEntrega = new Campo<string>();
+        public Campo<string> CEPEntrega = new Campo<string>();
+        public Campo<string> CidadeEntrega = new Campo<string>();
+        public Campo<string> EstadoEntrega = new Campo<string>();
+        public Campo<double> PrecoTotal = new Campo<double>();
+        public Campo<string> FormaPagto = new Campo<string>();
+        public Campo<string> Situacao = new Campo<string>();
+        public Campo<DateTime> DataPedido = new Campo<DateTime>();
+        public Campo<DateTime> DataEntrega = new Campo<DateTime>();
+    }   
+}
